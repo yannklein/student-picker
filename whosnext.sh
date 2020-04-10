fbbc559526a8824e644205044058fe26a7285b35
Yann Klein <yann.klein@me.com>
2020-04-10 10:08:46 +0900

    get list array of students feature

--- whosnext.sh
+++ whosnext.sh
@@ -3,9 +3,8 @@
 # function to join array element to string with separator
 function join_by { local IFS="${1}"; shift; echo "$*"; }
 
-if [ ${1+x} ]
-# if arg existing get the students in an array
-then
+
+function new_students() {
   if [ -e students.txt ]
   then
   # if existing erase the file content
@@ -16,10 +15,14 @@ then
   `touch students.txt`
   echo "tmp:$1" >> students.txt
   echo "list:$1" >> students.txt
-  echo "The new students list: ${1}"
+  echo " "
+  echo "The new students list:"
+  echo "${1}"
+  echo " "
 
-# if no arg run the program on existing students
-else
+}
+
+function pick_student {
   # read students from file
   while IFS='' read -r line
   do
@@ -68,5 +71,52 @@ else
   # add the new content
   echo "tmp:${tmp_joined_students}" >> students.txt
   echo "list:${list_joined_students}" >> students.txt
+}
+
+function students_list() {
+  # read students from file
+  while IFS='' read -r line
+  do
+    IFS=':' read -r -a line_array <<< "$line"
+    header=${line_array[0]}
+    content=${line_array[1]}
+    if [ $header == "list" ]
+    then
+      students=$content
+    fi
+  done < students.txt
+
+  IFS=',' read -r -a list_students <<< "${students}"
+
+  for i in $( seq 1 $1 )
+  do
+    rand_index=$[$RANDOM % ${#list_students[@]}]
+    if ! [ ${#list_students[@]} -eq 0 ]
+    then
+      picked_students+=("\"${list_students[$rand_index]}\"")
+      unset 'list_students[$rand_index]'
+      tmp_string=$(join_by , ${list_students[@]})
+      IFS=',' read -r -a list_students <<< "${tmp_string}"
+    fi
+  done
+  picked_students_string=$(join_by , ${picked_students[@]})
+  echo " "
+  echo "Here is an array of ${1} students:"
+  echo " [${picked_students_string}] "
+  echo " "
+}
+
+if ! [ ${1+x} ]
+# if no arg run the program on existing students
+then
+  pick_student
+
+elif [[ "$1" =~ ^[0-9]+$ ]]
+# if arg existing get the students in an array
+then
+ students_list ${1}
+else
+# if any other arguments (especially a comma spearated list)
+ new_students ${1}
 fi
 
